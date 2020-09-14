3f15e423bfec7639d2db03b8f1955bc5532d42ce
jairpro <jairedias@yahoo.com.br>
2020-09-14 19:47:13 -0300

    Update admins.sql
    Corrige DEFAULT 'NULL' para NULL do campo admins.name;

--- src/database/create_tables/admins.sql
+++ src/database/create_tables/admins.sql
@@ -12,7 +12,7 @@ CREATE TABLE `admins` (
   `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `hash` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT 'NULL',
+  `name` varchar(255) DEFAULT NULL,
   `user` varchar(255) DEFAULT NULL,
   `type` char(1) DEFAULT NULL,
   `status` char(1) DEFAULT NULL,
